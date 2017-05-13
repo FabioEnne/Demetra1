403a808bf44be3dda06279590543c65eab7214bb
Fabio Nardini <fabionardini84@gmail.com>
2017-05-13 18:58:10 +0200

    Minor UI change, added "Read seetings from server" service.

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,9 +1,11 @@
 ﻿import { Component } from '@angular/core';
+import { LoadingController } from 'ionic-angular';
 
 import { NavController } from 'ionic-angular';
 import * as io from 'socket.io-client';
 var a;
 var b;
+var wateringTimeDecod;
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
@@ -17,49 +19,39 @@ export class HomePage {
   rangeSettings:number;
   slideValueBadge = [];
 
-
-  constructor(public navCtrl: NavController) {
-   
-   this.socket = io('http://192.168.1.21:3000');
-      this.socket.on('message', (msg) => {
-     console.log("message", msg);
-     this.chats.push(msg);
-   });
-
-   this.socket.on('prova', (msgProva) => {
-       console.log("msgProva", msgProva);
-       a = msgProva;
-       this.rangeSettings = msgProva;
-       this.prova.push(msgProva);
-   });
-
-   this.socket.on('photo', (value) => {
-       console.log("Luminosità: ", value);
-       this.photoValue.push(value);
-   });
-
-   this.socket.on('sValue', (value) => {
-       this.slideValueBadge.push(value);
-       this.rangeSettings = value;
-       b = value;
-       console.log("sliderValue:", value);
-   });
+  
+  constructor(public navCtrl: NavController, public loadingCtrl: LoadingController) {
+     this.socket = io('http://2.35.113.223:3000');
+     this.socket.on('photo', (value) => {
+         console.log("Luminosità: ", value);
+         this.photoValue.push(value);
+     });
+     this.socket.on('sValue', (value) => {
+         this.slideValueBadge.push(value);
+         this.rangeSettings = value;
+         b = value;
+         console.log("sliderValue:", value);
+     });
+     this.socket.on('wateringTime', (value) => {
+         wateringTimeDecod = value / 1000;
+         this.slideValueBadge.push(wateringTimeDecod);
+         this.rangeSettings = wateringTimeDecod;
+         console.log("sliderValue:", value);
+     });
+  }
+  presentLoading() {
+      this.loadingCtrl.create({
+          content: 'Sto innafiando...',
+          duration: b*1000,
+          dismissOnPageChange: true
+      }).present();
   }
 
   slampa() {
       this.socket.emit('innafia', b);
+      this.presentLoading();
   }
 
-  stopslampa() {
-      this.socket.emit('stopslampa');
-  }
-
-  send(msg) {
-        if(msg != ''){
-            this.socket.emit('message', msg);
-        }
-        this.chat_input = '';
-  }
   cValue(event, nome) {
       console.log("SliderValue", event._valA);
       this.socket.emit('sValue', event._valA);
